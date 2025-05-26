acdaf8f034a1303687da44f882bc96e961c1ef9f
Badrul Ijazi <badrulijazi@gmail.com>
2025-05-26 11:09:53 +0800

    fix presenter not found

--- src/containers/Recommend.tsx
+++ src/containers/Recommend.tsx
@@ -1,14 +1,12 @@
-import { lazy, Suspense } from "react";
-import { Box } from "@mui/material";
 import { useStateProvider } from "../Utility/Reducer/StateProvider";
-const PresenterRecommend = lazy(() => import('../presenter/PresenterRecommend'));
+import PresenterRecommend from "../presenter/PresenterRecommend";
 
 export default function Recommend() {
     const { state } = useStateProvider()
 
     return (
-        <Suspense fallback={<Box></Box>}>
+        <>
             {state.recommend && state.recommend.length > 0 && <PresenterRecommend />}
-        </Suspense>
+        </>
     );
 }
